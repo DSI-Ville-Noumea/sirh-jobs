380be8d11739daac8be90559a3d666fb1c61f6f0
nicno85 <nicno85@ville-noumea.nc>
2014-05-13 22:19:24 +0000

    #2481 : Utiliser RADI pour interagir avec l'AD

--- src/main/java/nc/noumea/mairie/ldap/dao/IAgentLdapDao.java
+++
@@ -1,8 +0,0 @@
-package nc.noumea.mairie.ldap.dao;
-
-import nc.noumea.mairie.ldap.domain.AgentLdap;
-
-public interface IAgentLdapDao {
-
-	public AgentLdap retrieveAgentFromLdapFromMatricule(String agentId) throws AgentLdapDaoException;
-}
