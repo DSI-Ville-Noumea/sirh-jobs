8ecf50f4d89e27f416d5e7f2502d04ae753be876
nicno85 <nicno85@ville-noumea.nc>
2014-05-18 22:48:43 +0000

    MER du 22/05 version 5.1

--- src/site/resources/scripts/5.1-1-create.sql
+++ src/site/resources/scripts/5.1-1-create.sql
@@ -7,7 +7,7 @@ ID_AGENT INTEGER not null,
 LOGIN varchar(500),
 MAIL varchar(500),
 NOMATR INTEGER not null,
-constraint SIRH.PK_SIIDMA_SIRH
+constraint SIRH2.PK_SIIDMA_SIRH
 primary key (ID_AGENT)
 );
 
