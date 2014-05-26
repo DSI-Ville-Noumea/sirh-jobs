2b1e62183e48185b9e0ec585d72911a8a9f45c7a
nicno85 <nicno85@ville-noumea.nc>
2014-05-26 04:32:11 +0000

    #4200 : Exception du job : AvancementsWithEaesMassPrintJob en qualif --> ajout de logs

--- src/main/java/nc/noumea/mairie/sirh/dao/AvctCapPrintJobDao.java
+++ src/main/java/nc/noumea/mairie/sirh/dao/AvctCapPrintJobDao.java
@@ -7,6 +7,8 @@ import nc.noumea.mairie.sirh.domain.AvctCapPrintJob;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
@@ -14,36 +16,47 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class AvctCapPrintJobDao implements IAvctCapPrintJobDao {
 
+	private Logger logger = LoggerFactory.getLogger(AvctCapPrintJobDao.class);
+
 	@Autowired
 	@Qualifier("sirhSessionFactory")
 	private SessionFactory sirhSessionFactory;
-	
+
 	@SuppressWarnings("rawtypes")
 	@Override
 	public AvctCapPrintJob getNextPrintJob() {
 
-		if (!sirhSessionFactory.getCurrentSession().getTransaction().isActive())
-			sirhSessionFactory.getCurrentSession().beginTransaction();
-		
-		Query jobQuery = sirhSessionFactory.getCurrentSession()
+		logger.debug("Entrée fonction getNextPrintJob de AvctCapPrintJobDao");
+
+		Session session = sirhSessionFactory.getCurrentSession();
+
+		if (!session.getTransaction().isActive()) {
+			session.beginTransaction();
+			logger.debug("AvctCapPrintJobDao : la session n'est pas active");
+		}
+
+		Query jobQuery = session
 				.createQuery("select job from AvctCapPrintJob job where job.jobId is null order by job.submissionDate asc");
 		jobQuery.setMaxResults(1);
-		
+
 		List result = jobQuery.list();
-		
+		logger.debug("AvctCapPrintJobDao : taille de la liste result = " + result.size());
+
 		if (result.size() != 1)
 			return null;
-		
+
 		AvctCapPrintJob pj = (AvctCapPrintJob) result.get(0);
 
-		sirhSessionFactory.getCurrentSession().getTransaction().rollback();
-		
+		session.getTransaction().rollback();
+
+		logger.debug("Sortie fonction getNextPrintJob de AvctCapPrintJobDao");
+
 		return pj;
 	}
 
 	@Override
 	public void updateAvctCapPrintJob(AvctCapPrintJob job) {
-		
+
 		Session session = sirhSessionFactory.getCurrentSession();
 		session.beginTransaction();
 		session.merge(job);
