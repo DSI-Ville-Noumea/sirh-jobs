0c66a829a7a251e7f2733a9f4edf217e84e10f06
nicno85 <nicno85@ville-noumea.nc>
2014-08-17 21:04:28 +0000

    #10616 : nested transactions not supported --> ajout de logs

--- src/main/java/nc/noumea/mairie/sirh/dao/SirhDocumentDao.java
+++ src/main/java/nc/noumea/mairie/sirh/dao/SirhDocumentDao.java
@@ -7,12 +7,16 @@ import nc.noumea.mairie.sirh.ws.dto.LightUser;
 
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class SirhDocumentDao implements ISirhDocumentDao {
+
+	private Logger logger = LoggerFactory.getLogger(SirhDocumentDao.class);
 
 	@Autowired
 	@Qualifier("sirhSessionFactory")
@@ -33,6 +37,7 @@ public class SirhDocumentDao implements ISirhDocumentDao {
 
 	@Override
 	public void deleteSIIDMAEntries() {
+		logger.debug(" entrée deleteSIIDMAEntries with sirhSessionFactory " + sirhSessionFactory);
 
 		sirhSessionFactory.getCurrentSession().beginTransaction();
 
