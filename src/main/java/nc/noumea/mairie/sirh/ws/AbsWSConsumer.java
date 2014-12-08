467bb62cff7580cc630c94d4cd7fafd3b938168e
nicno85 <nicno85@ville-noumea.nc>
2014-12-08 02:34:22 +0000

    #11884 : CA --> routine de fin d'année

--- src/main/java/nc/noumea/mairie/sirh/ws/AbsWSConsumer.java
+++ src/main/java/nc/noumea/mairie/sirh/ws/AbsWSConsumer.java
@@ -14,7 +14,7 @@ import com.sun.jersey.api.client.ClientResponse;
 
 @Service
 public class AbsWSConsumer extends BaseWsConsumer implements IAbsWSConsumer {
-	
+
 	@Autowired
 	@Qualifier("SIRH_ABS_WS_Base_URL")
 	private String SIRH_ABS_WS_Base_URL;
@@ -22,80 +22,113 @@ public class AbsWSConsumer extends BaseWsConsumer implements IAbsWSConsumer {
 	@Autowired
 	@Qualifier("SIRH_ABS_WS_emailInformationUrl")
 	private String emailInformationUrl;
-	
+
 	@Autowired
 	@Qualifier("SIRH_ABS_WS_ListeCompteurAnneePrecedenteUrl")
 	private String listeCompteurAnneePrecedenteUrl;
-	
+
 	@Autowired
 	@Qualifier("SIRH_ABS_WS_ResetCompteurAnneePrecedenteUrl")
 	private String resetCompteurAnneePrecedenteUrl;
-	
+
 	@Autowired
 	@Qualifier("SIRH_ABS_WS_ListeCompteurAnneeEnCoursUrl")
 	private String listeCompteurAnneeEnCoursUrl;
-	
+
 	@Autowired
 	@Qualifier("SIRH_ABS_WS_ResetCompteurAnneeEnCoursUrl")
 	private String resetCompteurAnneeEnCoursUrl;
-	
+
+	@Autowired
+	@Qualifier("SIRH_ABS_WS_ListeCompteurCongeAnnuelUrl")
+	private String listeCompteurCongeAnnuelUrl;
+
+	@Autowired
+	@Qualifier("SIRH_ABS_WS_ResetCompteurCongeAnnuelUrl")
+	private String resetCompteurCongeAnnuelUrl;
+
 	@Override
 	public EmailInfoDto getListIdDestinatairesEmailInfo() {
-		
+
 		String url = String.format(SIRH_ABS_WS_Base_URL + emailInformationUrl);
 
 		Map<String, String> parameters = new HashMap<String, String>();
-		
+
 		ClientResponse res = createAndFireGetRequest(parameters, url);
 
 		return readResponse(EmailInfoDto.class, res, url);
 	}
-	
+
 	@Override
 	public List<Integer> getListeCompteurAnneePrecedente() {
-		
+
 		String url = String.format(SIRH_ABS_WS_Base_URL + listeCompteurAnneePrecedenteUrl);
 
 		Map<String, String> parameters = new HashMap<String, String>();
-		
+
 		ClientResponse res = createAndFireGetRequest(parameters, url);
 
 		return readResponseAsList(Integer.class, res, url);
 	}
-	
+
 	@Override
 	public ReturnMessageDto resetCompteurAnneePrecedente(Integer idAgentReposCompCount) {
-		
+
 		String url = String.format(SIRH_ABS_WS_Base_URL + resetCompteurAnneePrecedenteUrl);
 
 		Map<String, String> parameters = new HashMap<String, String>();
-			parameters.put("idAgentReposCompCount", String.valueOf(idAgentReposCompCount));
-		
+		parameters.put("idAgentReposCompCount", String.valueOf(idAgentReposCompCount));
+
 		ClientResponse res = createAndFireGetRequest(parameters, url);
 
 		return readResponse(ReturnMessageDto.class, res, url);
 	}
-	
+
 	@Override
 	public List<Integer> getListeCompteurAnneeEnCours() {
-		
+
 		String url = String.format(SIRH_ABS_WS_Base_URL + listeCompteurAnneeEnCoursUrl);
 
 		Map<String, String> parameters = new HashMap<String, String>();
-		
+
 		ClientResponse res = createAndFireGetRequest(parameters, url);
 
 		return readResponseAsList(Integer.class, res, url);
 	}
-	
+
 	@Override
 	public ReturnMessageDto resetCompteurAnneeEnCours(Integer idAgentReposCompCount) {
-		
+
 		String url = String.format(SIRH_ABS_WS_Base_URL + resetCompteurAnneeEnCoursUrl);
 
 		Map<String, String> parameters = new HashMap<String, String>();
-			parameters.put("idAgentReposCompCount", String.valueOf(idAgentReposCompCount));
-		
+		parameters.put("idAgentReposCompCount", String.valueOf(idAgentReposCompCount));
+
+		ClientResponse res = createAndFireGetRequest(parameters, url);
+
+		return readResponse(ReturnMessageDto.class, res, url);
+	}
+
+	@Override
+	public List<Integer> getListCompteurCongeAnnuel() {
+
+		String url = String.format(SIRH_ABS_WS_Base_URL + listeCompteurCongeAnnuelUrl);
+
+		Map<String, String> parameters = new HashMap<String, String>();
+
+		ClientResponse res = createAndFireGetRequest(parameters, url);
+
+		return readResponseAsList(Integer.class, res, url);
+	}
+
+	@Override
+	public ReturnMessageDto resetCompteurCongeAnnuel(Integer idAgentCongeAnnuelCount) {
+
+		String url = String.format(SIRH_ABS_WS_Base_URL + resetCompteurCongeAnnuelUrl);
+
+		Map<String, String> parameters = new HashMap<String, String>();
+		parameters.put("idAgentCongeAnnuelCount", String.valueOf(idAgentCongeAnnuelCount));
+
 		ClientResponse res = createAndFireGetRequest(parameters, url);
 
 		return readResponse(ReturnMessageDto.class, res, url);
--- src/main/java/nc/noumea/mairie/sirh/ws/IAbsWSConsumer.java
+++ src/main/java/nc/noumea/mairie/sirh/ws/IAbsWSConsumer.java
@@ -7,12 +7,16 @@ import nc.noumea.mairie.sirh.ws.dto.EmailInfoDto;
 public interface IAbsWSConsumer {
 
 	public EmailInfoDto getListIdDestinatairesEmailInfo();
-	
+
 	List<Integer> getListeCompteurAnneePrecedente();
-	
+
 	ReturnMessageDto resetCompteurAnneePrecedente(Integer idAgentReposCompCount);
 
 	List<Integer> getListeCompteurAnneeEnCours();
-	
+
 	ReturnMessageDto resetCompteurAnneeEnCours(Integer idAgentReposCompCount);
+
+	List<Integer> getListCompteurCongeAnnuel();
+
+	ReturnMessageDto resetCompteurCongeAnnuel(Integer idAgentCongeAnnuelCount);
 }
