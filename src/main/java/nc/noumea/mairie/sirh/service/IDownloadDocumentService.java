513bb706e84fbf17889e4753d3c317b8d561f0e7
noemienicolas <noemie.nicolas54@gmail.com>
2017-06-28 13:29:34 +1100

    #40268 : [TR] erreur à chaque etat payeur

--- src/main/java/nc/noumea/mairie/sirh/service/IDownloadDocumentService.java
+++ src/main/java/nc/noumea/mairie/sirh/service/IDownloadDocumentService.java
@@ -3,6 +3,8 @@ package nc.noumea.mairie.sirh.service;
 import java.util.List;
 import java.util.Map;
 
+import com.sun.jersey.api.client.ClientResponse;
+
 public interface IDownloadDocumentService {
 
 	public void downloadDocumentToLocalPath(String url, Map<String, String> urlParameters, String localPath) throws Exception;
@@ -11,6 +13,12 @@ public interface IDownloadDocumentService {
 
 	public <R> List<R> downloadJsonDocumentAsList(Class<R> resultClass, String url, Map<String, String> urlParameters) throws Exception;
 
-	public <R> R postAs(Class<R> resultClass, String url, Map<String, String> urlParameters) throws Exception;
+	public <R> R postAs(Class<R> resultClass, String url, Map<String, String> urlParameters) throws Exception;	
+	
+	public ClientResponse createAndFireRequest(String url, Map<String, String> urlParameters) throws Exception;
+
+	public <T> T readResponse(Class<T> targetClass, ClientResponse response, String url) throws Exception;
+
+
 
 }
